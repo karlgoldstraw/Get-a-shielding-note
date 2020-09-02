8f587c444472e1b7cb477c0943cdab6d0f33e746
AdamChrimes <iChrimes@live.co.uk>
2020-09-02 11:34:34 +0100

    add some validation examples and content

--- app/validation-rules.js
+++ app/validation-rules.js
@@ -3,5 +3,25 @@ const { body } = require('express-validator');
 // Guidance at: docs/views/examples/validation
 
 module.exports = {
-  // Add rules here, examples at: docs/documentation_validation_rules.js
+  '/v1/screener-question/answer': [
+    body('self-isolate')
+      .not().isEmpty()
+      .withMessage('Select if you are at high risk from coronavirus (clinically extremely vulnerable)')
+  ],
+  '/v1/screener-question-live-work/answer': [
+    body('self-isolate-live-work')
+      .not().isEmpty()
+      .withMessage('Select if you work in an area where shielding advice is in place, but live outside this area')
+  ],
+  '/v1/screener-question-leave-home/answer': [
+    body('self-isolate-leave-home')
+      .not().isEmpty()
+      .withMessage('Select if you need to leave you home to go to work ')
+  ],
+  '/v1/self-isolate-note-when/answer': [
+    body('self-isolate-note-when')
+      .not().isEmpty()
+      .withMessage('Select when you need your shielding note to start from')
+  ],
 };
+
