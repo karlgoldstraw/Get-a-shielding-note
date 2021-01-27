f40d0c59ad0c9bc07745528289ab8c17d76ef245
AdamChrimes <iChrimes@live.co.uk>
2021-01-27 12:09:20 +0000

    content updates and remove question

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // Add your routes here - above the module.exports line
 
 // First screener page routing
-router.post('/*/screener-question/answer', function (req, res) {
+router.post('/v1/screener-question/answer', function (req, res) {
   // Set a variable to the answer
   var answer = req.session.data['self-isolate']
   // Set prototype version
@@ -14,18 +14,18 @@ router.post('/*/screener-question/answer', function (req, res) {
   // Check whether the answer is yes
   if (answer == "yes"){
     // If the answer is yes send to the next question
-    res.redirect(`/${version}/screener-question-live-work`);
+    res.redirect(`/v1/screener-question-live-work`);
   }
   else {
     // If the answer is no send to the no screen
-    res.redirect(`/${version}/screener-question-no`);
+    res.redirect(`/v1/screener-question-no`);
   }
 
 })
 
 
 // Second screener page routing
-router.post('/*/screener-question-live-work/answer', function (req, res) {
+router.post('/v1/screener-question-live-work/answer', function (req, res) {
   // Set a variable to the answer
   var answer = req.session.data['self-isolate-live-work']
   // Set prototype version
@@ -34,11 +34,11 @@ router.post('/*/screener-question-live-work/answer', function (req, res) {
   // Check whether the answer is yes
   if (answer == "yes"){
     // If the answer is yes send to the next question
-    res.redirect(`/${version}/screener-question-leave-home`);
+    res.redirect(`/v1/screener-question-leave-home`);
   }
   else {
     // If the answer is no send to the no screen
-    res.redirect(`/${version}/screener-question-no`);
+    res.redirect(`/v1/screener-question-no`);
   }
 
 })
@@ -84,7 +84,24 @@ router.post('/*/self-isolate-note-when/answer', function (req, res) {
 
 })
 
+// First screener page routing
+router.post('/v2/screener-question/answer', function (req, res) {
+  // Set a variable to the answer
+  var answer = req.session.data['self-isolate']
+  // Set prototype version
+  var version = req.params[0];
 
+  // Check whether the answer is yes
+  if (answer == "yes"){
+    // If the answer is yes send to the next question
+    res.redirect(`/v2/screener-question-leave-home`);
+  }
+  else {
+    // If the answer is no send to the no screen
+    res.redirect(`/v2/screener-question-no`);
+  }
+
+})
 
 
 module.exports = router;
